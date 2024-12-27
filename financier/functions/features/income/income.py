1634901ce1bdb34da864d00bd9a3c30964ab00be
Ariel Catli <dev.arielcatli@gmail.com>
2024-12-27 11:58:14 +0800

    refactor(income): streamline income retrieval query for clarity

--- financier/functions/features/income/income.py
+++ financier/functions/features/income/income.py
@@ -19,7 +19,5 @@ async def list_incomes(
 
 @income_router.get("/{income_id}")
 async def get_income(income_id: str, user_id: str):
-    income = IncomeDB.income_id_index.query(
-        user_id, IncomeDB.id == income_id
-    ).next()
+    income = IncomeDB.income_id_index.query(user_id, IncomeDB.id == income_id).next()
     return income.attribute_values
